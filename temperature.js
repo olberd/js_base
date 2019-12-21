22ca8c56796a251603db0f61a5c08f203ec89ec3
Oleg <olberd@bk.ru>
2019-12-21 13:33:00 +0300

    lesson_1

---
+++ temperature.js
@@ -0,0 +1,14 @@
+"use strict";
+var Tf;
+var Tc = prompt('Введите температуру по Цельсию ')
+Tf = (9 / 5) * Tc + 32;
+alert(Tf);
+
+//
+var admin;
+var name = "Василий"; 
+admin = name;
+alert(admin);
+
+// 1000 + "108" = 1000108
+
